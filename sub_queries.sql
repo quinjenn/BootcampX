b219877cfe932b413c23f135973ad987c95a6d7f
quinjenn <jquintal_@hotmail.com>
2023-03-06 19:10:02 -0800

    added sub queries

---
+++ sub_queries.sql
@@ -0,0 +1,31 @@
+// SUB QUERY EXAMPLE
+
+SELECT (
+  SELECT count(assignments)
+  FROM assignments
+)-count(assignment_submissions) as total_incomplete
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
+
+// FROM SUB QUERY EXAMPLE
+
+SELECT avg(total_students) as average_students
+FROM (
+  SELECT count(students) as total_students, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+) as totals_table;
+
+// IN SUB QUERY EXAMPLE
+
+SELECT assignments.name
+FROM assignments 
+WHERE id NOT IN
+(
+  SELECT assignment_id
+  FROM assignment_submissions
+  JOIN students ON students.id = student_id
+  WHERE students.name = 'Ibrahim Schimmel'
+);
