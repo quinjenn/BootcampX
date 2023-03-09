72715cbfc47d264cfa2c1c047e5b4c409a1efdef
quinjenn <jquintal_@hotmail.com>
2023-03-09 13:32:41 -0800

    SQL Injection - Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,6 +9,25 @@ const pool = new Pool({
 });
 
 // query using node-postgres
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     })
+//   });
+
+// parameterized queries
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -17,7 +36,7 @@ JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
